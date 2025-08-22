b03f5b2f02647e587fe68d3ea46795857b786620
platon <117759287+windusik@users.noreply.github.com>
2025-08-22 20:50:57 +0500

    edit start.bat for new upadte

--- start.bat
+++ start.bat
@@ -1,3 +1,9 @@
 @echo off
+chcp 65001 > nul
+title ZipUnlocker v3
+echo ===============================
+echo    ZipUnlocker v3 - Final Release
+echo ===============================
+echo.
 pip install -r requirements.txt
 python main.py
